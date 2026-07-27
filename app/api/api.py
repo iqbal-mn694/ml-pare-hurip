b6af5a4d398a94b4c9c2f54846541873f2e656bb
iqbal-mn694 <garenahiber134@gmail.com>
2026-07-27 20:50:58 +0700

    add comment to clarify routes in API router

--- app/api/api.py
+++ app/api/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.routers import health, markov, random_forest
 from app.core.config import get_settings
 
+# routes
 api_router = APIRouter(prefix=get_settings().api_v1_prefix)
 api_router.include_router(health.router)
 api_router.include_router(markov.router)
