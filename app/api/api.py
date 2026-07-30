00b822821b592e02bb6b19b1946311c8e31c3822
iqbal-mn694 <garenahiber134@gmail.com>
2026-07-30 08:49:56 +0700

    add rice price prediction routes

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routers import health, markov, random_forest
+from app.api.routers import health, markov, random_forest, naive_price, lstm_hybrid_price
 from app.core.config import get_settings
 
 # routes
@@ -8,3 +8,5 @@ api_router = APIRouter(prefix=get_settings().api_v1_prefix)
 api_router.include_router(health.router)
 api_router.include_router(markov.router)
 api_router.include_router(random_forest.router)
+api_router.include_router(naive_price.router)
+api_router.include_router(lstm_hybrid_price.router)
